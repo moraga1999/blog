10febb1459e4f3337ce7c4396b4518e045c74f61
Javier Moraga <javiermoragav@gmail.com>
2024-08-06 23:20:47 -0400

    feat: included sorbet and tailwind gems

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "javi", password: "secret", except: [:index, :show]
   
   def index
     @articles = Article.all
